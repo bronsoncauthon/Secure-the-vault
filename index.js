e4bed4c14f39a2dbedf21e5e52aa987fd6752fe4
Bronson Cauthon <bronsoncauthon17@gmail.com>
2026-09-14 14:37:00 -0400

    Finished workshop

---
+++ index.js
@@ -0,0 +1,13 @@
+let code1;
+
+code1 = 7 + 3;
+
+let code2 = 20 + 23 - 3;
+
+let code3 = (39 % 19) + 38;
+
+const message = "the vault has been secured. The combination is:";
+
+const codeA = code1 + "-" + code2 + "-" + code3;
+
+const codeB = `${code1}-${code2}-${code3}`;
